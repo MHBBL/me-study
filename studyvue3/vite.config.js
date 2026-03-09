8733e8e117eacffb440cdd255f2f56a6f9d2e7cb
MHBBL <3625187632@qq.com>
2026-03-09 18:01:55 +0800

    修复vite配置逗号

--- studyvue3/vite.config.js
+++ studyvue3/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/me-study/'
+  base:'/me-study/',
   plugins: [
     vue(),
     vueDevTools(),
